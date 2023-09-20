fce8c8707fbbb235d79e0fb7506a838b274bb920
aloepker <aloepker@gmail.com>
2023-09-20 14:52:32 -0500

    Commit # 2
    Updated the code structure.
    
    Started defining variables to be used as well.
    
    Steady as she goes!

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.util.*;
+
 public class Main {
+//variable calls
+public static int charClass, lexLen, token=10, nextToken, i;
+public static char nextChar;
+public static char[] lexeme = new char[100];
+public static FileReader in_fp;
+
+    public static void addChar(){
+
+    }
+
+    public static void getChar() {
+
+    }
+
+    public static void getNonBlank(){ //a function to call getChar until it returns a non-whitespace character
+
+    }
+
+    public static int lex(){
+        i=0;
+        System.out.println(i);
+        return i;
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("That's it! That's the melody to funky town!!\n");
+
+
+        System.out.println("\nThat's it! That's the melody to funky town!!\n");
 
     }
 }
