d8f9b9358e9d7e28be3451b7072abd9de791f4d7
aloepker <aloepker@gmail.com>
2023-09-21 21:10:46 -0500

    Commit # 8
    Finished!
    Code segment has been cleaned!
    Code Runs as Expected!
    Ready to turn in!

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 import java.io.*;
-import java.util.Arrays;
-//import static java.lang.Character.isAlphabetic;
-//import static java.lang.Character.isDigit;
-//import java.util.*;  not used
-
 public class Main {
 //variable calls
-    public static int charClass, lexLen, nextToken; //token variable not used
+    public static int charClass, lexLen, nextToken;
     public static char nextChar;
-    //public static char[] lexeme = new char[6]; //from 100
     public static String lex;
     public static FileReader in_fp;
     public static final int LETTER = 0;
@@ -25,8 +19,8 @@ public class Main {
     public static final int RIGHT_PAREN = 26;
     public static final int EOF = -1;
 
-    public static void lookup(char ch){   // apparently when this function is called, it is not looking for return data.. so I changed it to void from int
-        lex = "";    // experimental code!
+    public static void lookup(char ch){
+        lex = "";
         switch(ch){
             case '(':
                 addChar();
@@ -61,31 +55,11 @@ public class Main {
                 nextToken = EOF;
                 break;
         }
-        //return nextToken; not needed with void return type
     }
-
     public static void addChar(){
-       // if (lexLen<= 98){
-           // lexeme[lexLen++] = nextChar;
-           // lexeme[lexLen] = 0;
-
-       // } else {
-        //    System.out.println("Error - lexeme is too long\n");
-      //  }
         lex = lex + nextChar;
     }
-
     public static void getChar() {
-      /*  if ((nextChar = get c(in_fp)) = EOF) {
-            if (isAlphabetic(nextChar))
-                charClass = LETTER;
-            else if (isDigit(nextChar))
-                charClass = DIGIT;
-            else charClass = UNKNOWN;
-        }
-        else
-            charClass = EOF;
-       */
         try {
             int nextCharInt = in_fp.read();
             if (nextCharInt == -1) {
@@ -103,19 +77,14 @@ public class Main {
         } catch (IOException e) {
             charClass = EOF;
         }
-
-
     }
-
-    public static void getNonBlank(){ //a function to call getChar until it returns a non-whitespace character
+    public static void getNonBlank(){
         while (Character.isWhitespace(nextChar)) {
             getChar();
         }
     }
-
-    public static void lex(){ //switching from into to void return type
+    public static void lex(){
         lexLen = 0;
-        //this might be the ideal spot to add: lex="";
         lex="";
         getNonBlank();
         switch(charClass) {
@@ -128,7 +97,6 @@ public class Main {
                 }
                 nextToken = IDENT;
                 break;
-            /* Parse integer literals */
             case DIGIT:
                 addChar();
                 getChar();
@@ -138,42 +106,26 @@ public class Main {
                 }
                 nextToken = INT_LIT;
                 break;
-            /* Parentheses and operators */
             case UNKNOWN:
                 lookup(nextChar);
                 getChar();
                 break;
-            /* EOF */
             case EOF:
                 nextToken = EOF;
-              //  lexeme[0] = 'E';
-              //  lexeme[1] = 'O';
-              //  lexeme[2] = 'F';
-              //  lexeme[3] = 0;
                 lex = "EOF";
                 break;
-        } /* End of switch */
-        System.out.println("Next token is: "+nextToken+", Next lexeme is "+ lex +"\n"); //added tostring code    removed: Arrays.toString(lexeme)
-        //return nextToken; not needed with void return type
-    } /* End of function lex */
-    public static void main(String[] args) {  // wrap in a try catch, see code translation
-       /* System.out.println("\nThat's it! That's the melody to funky town!!\n");
-        if ((in_fp = f open("front.in", "r")) == Null) {
-            System.out.println("Error - Cannot open front.in\n");
-        }else{
-            getChar();
-            do{lex();}while(nextToken != EOF);
-        }*/
+        }
+        System.out.println("Next token is: "+nextToken+", Next lexeme is "+ lex +"\n");
+        }
+    public static void main(String[] args) {
         try {
-            in_fp = new FileReader("test.txt"); //from front.in
+            in_fp = new FileReader("front.in");
             getChar();
             do {
                 lex();
             } while (nextToken != EOF);
         } catch (IOException e) {
-            System.out.println("ERROR - cannot open test.txt"); //from front.in
+            System.out.println("ERROR - cannot open front.in");
         }
-        //let's see if this bs works, lol
-        System.out.println(":D  That's it!! That's the melody to funky town!!!");
     }
 }
